d0be082e1564cb493f18cb8798c4fcdda2b02c44
GustiSchonfeld <1715373@ucc.edu.ar>
2025-02-27 18:09:16 -0300

    Front

---
+++ frontend/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './App.css';
+import AddContact from './AddContact';
+import axios from 'axios';
+
+function App() {
+  const [contactos, setContactos] = useState([]);
+
+  const fetchContacts = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/contactos');
+      setContactos(response.data);
+    } catch (error) {
+      console.error('Error al obtener los contactos:', error);
+      alert('Hubo un error al obtener los contactos.');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1 className="text-center">Agenda de Contactos</h1>
+      <AddContact />
+      
+      {/* Botón para obtener la lista de contactos */}
+      <button onClick={fetchContacts}>Mostrar Agenda de Contactos</button>
+
+      {/* Lista de contactos */}
+      {contactos.length > 0 && (
+        <ul>
+          {contactos.map((contacto) => (
+            <li key={contacto.id}>
+              {contacto.nombre} - {contacto.telefono} - {contacto.email}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
